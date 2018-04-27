4c716e5bc19dec91beb28daf6839b08e142e534d
okputadora <okputadora@gmail.com>
2018-04-26 20:34:53 -0400

    get requests for table data

--- server.js
+++ server.js
@@ -22,6 +22,14 @@ app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
 });
 
+app.get("/api/tables", function(req, res){
+  res.json(dataArray)
+})
+
+app.get("/api/waitlist", function(req, res){
+  res.json(waitList)
+})
+
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
@@ -36,19 +44,18 @@ app.post("/api/tables", function (req, res) {
         email: req.body.customerEmail,
         customerID: req.body.customerID
     };
-    if (dataArray.length < 5){
-    dataArray.push(newReservation);
-    console.log(dataArray);
-    res.json(newReservation);
-
+    if (dataArray.length < 2){
+      dataArray.push(newReservation);
+      console.log(dataArray);
+      res.json(newReservation);
     } else{
         waitList.push(newReservation);
         console.log(waitList);
         res.end();
     }
-    
-    
-   
+
+
+
 });
 
 
